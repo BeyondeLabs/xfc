dd5fcbd0036fa2bfb6f48b159cce4f2f0e04454c
ProfNandaa <profnandaa@gmail.com>
2014-11-15 05:03:23 +0300

    Signup form UI

--- app/application/views/home/register.php
+++ app/application/views/home/register.php
@@ -10,14 +10,24 @@
 		echo validation_errors('<div class="alert alert-danger" role="alert">','</div>');
 
 		echo form_open("home/register/submit","class='form'");
-		echo form_input("username",set_value("username"));
-		echo form_label("Username - <em>Short name or initials</em>","username");
+		echo form_input("first_name",set_value("first_name"),"class='half'");
+		echo form_input("last_name",set_value("last_name"),"class='half'");
+		echo form_label("First Name <span class='right'>Last Name</span>","first_name");
 		echo form_input("email",set_value("email"));
 		echo form_label("Email","email");
-		echo form_password("password",set_value("password"));
-		echo form_label("Password","password");
-		echo form_password("password_confirm",set_value("password_confirm"));
-		echo form_label("Confirm Password");
+		echo form_dropdown();
+		echo form_label("Christian Union","cuid");
+		echo form_dropdown("atid",array(),"", "class='half'");
+		echo form_dropdown("grad_year",array(),"","class='half'");
+		echo form_label("Affiliation <span class='right'>Year of Graduation</span>","atid");
+		echo form_input("phone",set_value("phone"));
+		echo form_label("phone","phone");
+
+		echo form_input("email",set_value("email"));
+		echo form_label("Email","email");
+		echo form_password("password",set_value("password"),"class='half'");
+		echo form_password("password_confirm",set_value("password_confirm"),"class='half'");
+		echo form_label("Password <span class='right'>Password Confirm</span>","password");
 
 		echo form_submit("register","Register","class='btn btn-lg btn-success'");
 
