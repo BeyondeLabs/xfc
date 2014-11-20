a5d657180df01bedbda127e214bc53281d396cb9
ProfNandaa <profnandaa@gmail.com>
2014-11-20 23:02:22 +0300

    Admin can view list of champions, close #24

--- app/application/views/admin/champions_view.php
+++ app/application/views/admin/champions_view.php
@@ -10,7 +10,7 @@ foreach($champs->result() as $row):
 <h4><?php echo $row->first_name." ".$row->last_name; ?></h4>
 <div class="desc"><?php echo $row->cu_name." &mdash; ".$row->uni_name; ?><br/>
 <i class="fa fa-chain"></i> <?php echo $row->at_name; ?><br/>
-<?php echo "KES. ".number_format($row->amount)." &mdash; ".$row->ct_name; ?>
+<?php echo "KES. ".number_format($row->amount)." &bull; ".$row->ct_name; ?>
 </div>
 
 </div>
