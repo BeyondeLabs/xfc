66d0c4fe860ca47c5ce9e90a7434ad05bda7d318
ProfNandaa <profnandaa@gmail.com>
2014-11-21 09:31:09 +0300

    Enhanced details on the list of champions

--- app/application/views/admin/champions_view.php
+++ app/application/views/admin/champions_view.php
@@ -6,10 +6,12 @@
 <?php
 foreach($champs->result() as $row):
 ?>
-<div class="col-md-2 c-profile">
+<div class="col-md-3 c-profile">
 <h4><?php echo $row->first_name." ".$row->last_name; ?></h4>
-<div class="desc"><?php echo $row->cu_name." &mdash; ".$row->uni_name; ?><br/>
-<i class="fa fa-chain"></i> <?php echo $row->at_name; ?><br/>
+<div class="desc">
+<i class="fa fa-clock-o"></i> Joined <?php echo $row->joined; ?><br/>
+<?php echo $row->cu_name." &mdash; ".$row->uni_name; ?><br/>
+<i class="fa fa-tag"></i> <?php echo $row->at_name." &bull; ".$row->grad_year; ?><br/>
 <?php echo "KES. ".number_format($row->amount)." &bull; ".$row->ct_name; ?>
 </div>
 
