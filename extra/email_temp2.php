1607705115702e3aa6b340176fee50f332a2c6d8
@ProfNandaa <profnandaa@gmail.com>
2015-01-10 06:06:36 +0300

    Added new sample email template

---
+++ extra/email_temp2.php
@@ -0,0 +1,13 @@
+<table style="width:100%;margin:0;padding-top:60px;padding-bottom:60px">
+            <tbody><tr style="width:100%;margin:0;padding:0">
+                <td style="width:100%;text-align:center;vertical-align:middle;height:100%;margin:0 auto;padding:0 100px" valign="middle" align="center"><h1 style="margin:0 auto;font-family:'Helvetica','Arial',sans-serif"><b><span style="color:#8b5789">THANK YOU</span></b><br><span>FOR YOUR INTEREST!</span></h1></td>
+            </tr>
+            <tr style="width:100%;margin:0;padding:0px">
+                <td style="width:100%;text-align:center;vertical-align:middle;height:100%;margin:0 auto;padding-top:30px;padding-bottom:30px;padding-left:100px;padding-right:100px" valign="middle" align="center">
+                    <p style="margin:0 auto;color:#aaa;font-size:14px;font-family:'Helvetica','Arial',sans-serif">We are hard at work finishing the platform that will allow a new form of 21st century volunteering. If you have any questions, comments, or a great idea please contact us - your feedback is appreciated. We will keep you posted about our progress.</p>
+                </td>
+            </tr>
+            <tr style="width:100%;margin:0;padding:0">
+                <td style="width:100%;text-align:center;vertical-align:middle;height:100%;margin:0 auto;padding:0" valign="middle" align="center"><img class="CToWUd" src="https://ci4.googleusercontent.com/proxy/KKqhilxx6Tg42Z8eNOrcsiC4-Je--xCDgHL5YYrug4ME6B0Gsxw7L5MrCau6M-yoOR95VAV9TmQSJQtJMilpmQO8=s0-d-e1-ft#http://hashtagcharity.org/img/logo_small.png" href="http://hashtagcharity.org"></td>
+            </tr>
+        </tbody></table>
