510f3577db4d5102461734553a8ec6d64b64d6c9
@ProfNandaa <profnandaa@gmail.com>
2015-01-16 12:32:08 +0300

    Added another sample email template (Udacity)

---
+++ extra/email_temp3.php
@@ -0,0 +1,195 @@
+
+<table width="100%" cellpadding="0" cellspacing="0" bgcolor="#ffffff">
+	<tbody><tr>
+		<td>
+			<table width="600" cellpadding="0" cellspacing="0" align="center">
+				<tbody><tr>
+					<td style="padding:12px 30px">
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tbody><tr>
+								<td style="font:11px/12px Arial,Helvetica,sans-serif;color:#4e4e4e">
+									Start Learning
+								</td>
+								<td style="font:11px/12px Arial,Helvetica,sans-serif;color:#4e4e4e" align="right">
+									
+								</td>
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+				<tr>
+					<td style="padding:12px 30px 10px" bgcolor="#354b59">
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tbody><tr>
+								<td style="line-height:0">
+									<a href="http://mailer.udacity.com/wf/click?upn=2RL1KjfO9zPBjFPLhnA72ItT5FTnMRB05CHta8yOCiMx4WWI3fwHY69VFfa88tT9HJe7JYlvWqf0eANEZ48SDX858OmOmYuqfC1EsrrMyc39eneQIedWYF9H6qhRT10i_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgQgwfwZhyR1ZJ-2FkL-2BaNPJCfaXGnwJ5AkP4GAWaZ-2FMBU-2Bs5Ho-2BYSfATGqIZCZFSH54igXx0XTyEXpGuh5NLwb-2FZoyvIugKV2gpOlXzCtlgymZqT5ko3RYMo1HbzlTrsYFyzkleQMrKkigNkP1OmxQxU-2B8BJajLZXhj-2FmOAcVoTm6Z" target="_blank"><img class="CToWUd" src="https://lh6.ggpht.com/kdoeTfUXmRf7rPEqS05Ml8x89kuMjvZPkc4IQ2qmhF3WXweMznboIX4xzSnJZMnmXhuo9FZJEKdM4IL4fA=s0#w=96&amp;h=17" alt="Udacity" height="17" hspace="0" vspace="0" width="96" align="left" border="0"></a>
+								</td>
+								<td style="font:11px/12px Arial,Helvetica,sans-serif;color:#fff" align="right">
+
+									<a style="color:#fff" href="http://mailer.udacity.com/wf/click?upn=2RL1KjfO9zPBjFPLhnA72HNlP0LY4JEoN1TALgaNwKjwk2njYCD2Si-2FCUVb0xcDA4-2F0EjJg69dy9f-2BYs4KZgidAgohOAxjwnF9s59v1G2UBevHMaxkvyCcr6qQYyqZVo79pmg0s3D-2BWzgwlMztFfcACdh4Z-2FA-2FpbnjSNG4tlRQw-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgSZW-2FItLTZpx6xQkinYVv1M4XMNxbbiAN0bdTCuYOrJzhXqqzpBhT1hNLHxg5x3Ds-2FGZNmHVFextUwiafEgXX0rgjBzHeU1YL3tc-2BcRRzJdokmBGRacfYNaQwGA7oWXO-2FzaV4L1Oodi2S536VRfUJ-2FpjMvzaNZrrgDvJXLijZ0yF" target="_blank">Sign in</a> or <a style="color:#fff" href="http://mailer.udacity.com/wf/click?upn=2RL1KjfO9zPBjFPLhnA72HNlP0LY4JEoN1TALgaNwKjwk2njYCD2Si-2FCUVb0xcDA4-2F0EjJg69dy9f-2BYs4KZgidAgohOAxjwnF9s59v1G2UBevHMaxkvyCcr6qQYyqZVo79pmg0s3D-2BWzgwlMztFfcAS6E8X2-2Fj1DLABdQZp2Zhk-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgbX5WyoBxxvVGctcXgBhnvi9lvVPz1gwomr9AN8ryQUejQgQ4lpt2UDtMwCGbLHxRyZQD5gVWucz1slCCk18CUYbeouCt6lQubxG256zvwzgUB-2Bf1IhItMQoyeUFUrkBcIsYQeDi0Uthizep5UuLzA0le0eh6eH6DHZstGGLWgFK" target="_blank">Sign up</a>
+
+								</td>
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+				
+<tr>
+	<td style="font:18px/20px Arial,Helvetica,sans-serif;color:#4d4d4d;padding:24px 30px 18px" bgcolor="#f5f5f5">
+		Programming Foundations with Python
+	</td>
+</tr>
+
+				<tr>
+					<td style="padding:16px 30px 0">
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tbody><tr>
+								<td style="font:14px/21px Arial,Helvetica,sans-serif;color:#363636;padding:29px 0 13px">
+									<b>Hi Anthony,</b><br><br>
+									
+
+
+You now have free access to the course materials!<br><br>
+
+In this class you will learn an important programming technique called Object Oriented Programming. 
+This will allow you to share and reuse code almost like Lego blocks.<br>
+<br>
+But we would be disappointed if that were the only outcome of this course. We want your work to be a 
+source of joy for others. In that spirit, we will work together to build 6 projects that will spread 
+happiness to those around you.
+<br><br>
+
+The course material is open, meaning you can take it at your own pace. So start learning and stay Udacious!
+<br><br>
+
+								</td>
+							</tr>
+							
+<tr>
+	<td style="padding:0 0 24px">
+		<a href="http://mailer.udacity.com/wf/click?upn=2RL1KjfO9zPBjFPLhnA72NHnaq5GfnRyVURD2ZvfzjgfU5vIDZAFUYNXnF623ER052ZVlbkt4MVx2p6-2BXE80wKBqVcTVgqg-2BXfBktS6pHg0jONBPItOJcJ1MXvtQvwUyc3F4ozGEyuNmCLcdQtPuoMM-2FjEJ6N07l2UbWWvPE0yc-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgU4tXqB-2F6b6xqgKpxeQQ4U7x-2BnOIxZX3uzwAY-2Frae-2FsulwjLRoCZBoCPE6HTuATq-2FWuEa3mqfBj5UphYJdXrjb29xQl-2BikdO4iAprKyFCGBIMAyFxLpawGMcIuD6aTqupCYiSaCu18zfuKkbdVf1UM5Jn2m3QnUHs6-2F-2FHRdYjYoV" target="_blank"><img class="CToWUd" src="https://lh5.ggpht.com/RHz3-01l7AZDrulgFpjwTA64Rwov2GLMGZ7pm1LEnbYq_UL2fXnDIOH4RaaJ65Xnr0vKfaB-yTN0GuPV-Sc=s0#w=160&amp;h=45" style="vertical-align:top" alt="Go to Class" height="45" width="161" border="0"></a>
+	</td>
+</tr>
+
+							<tr>
+								<td style="font:14px/16px Arial,Helvetica,sans-serif;color:#363636;padding:0 0 14px">
+									Cheers,
+								</td>
+							</tr>
+							<tr>
+								<td style="font:bold 14px/16px Arial,Helvetica,sans-serif;color:#363636;padding:0 0 7px">
+									The Udacity Team
+								</td>
+							</tr>
+							<tr>
+								<td style="padding:0 0 40px">
+									<img class="CToWUd" src="https://lh5.ggpht.com/0sgz0JpmR4KY_9Gkx26hZy5_8XgzvM3WoySgV9tFlwvxxcVAxF21VytHayQA7dEZiOiZqJr2a3MoCiq1Pg=s0#w=71&amp;h=76" style="vertical-align:top" alt="Udacity" height="76" width="71" border="0">
+								</td>
+							</tr>
+							<tr>
+								<td style="padding:0 0 20px">
+									
+	Share this class with friends:<br>
+	<a alt="Facebook" href="http://mailer.udacity.com/wf/click?upn=gmJQQrAKvQFtEwVr-2BF-2BO3nX4m9yED9TpfCn2B-2FbpX0wT02rlGxO1mTV7du71YM7B3dAIMPYLXIW1shH-2FmJH45O7DaOBiywPQ-2BYZ2LKFIPmeQy8F-2B2q-2FkaSOFYXAwu9tzdO4Su76se8qKp5maWGeDwShmndCXHM-2FZSMDRtfYhiMwskgLDAoyVBS5Vzqva6fiD-2FqKymIrX4BzgLtCKXhSrmw-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgdh9epebnN2BoLf3oAlBCgvgqwdsrDr7tNOkC7va7VmeNVOmkPyLX8HedDtq-2Bhu18yjw5Z-2BWSmp5j8NBg-2BjnUYCPZVi7oUdQbl99Gq31F9kkMayjn-2F7tVZlQFE6B7vTdX9-2FOoPFdBpSrKO5HhC7-2BqijOVSX3qyNDYKS-2FQOS9AMjG" target="_blank"><img class="CToWUd" src="https://lh4.ggpht.com/JP0SxD5OiWURYjT4rJGH_yU3FLctKqan73qGhXNwnKeGe6pImmLBplbX-aLXQRVMf8m3pUDLLZWoB_X7Aw=s0#w=22&amp;h=22" style="margin-right:6px"></a><a alt="Twitter" href="http://mailer.udacity.com/wf/click?upn=U2TTcdclIjca5RQ2dot-2FwDbNt1OU1dvMb9v1iU76cB7xg6Ov1X-2B2uH-2FbjpMdHzcjTSIMZaQULlVNLMLtfwBPDhhM0wp4c80IfytoUfKBq5snE654BfyqiIebO16OTppv33-2BF8W6eD36mDszw1F-2F4mvAq-2B0h11hyM0UtlQb9-2BitLZDJ55XUAy2PraEPQiTajp8qLuhmvdqVmFRe94cZdCI5FBJC9NnsnUsf6MJ5n66jqx4JY2lJLiB1ZmnWKF4k90OWp3R5Wu9f8nFTI7gP8Gp2mF5BKm9UwelhvlbIet49igwX3LUV-2FoZyxqTWzSrIeu_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgYWtCMs-2F58UihRqSK8ROSFv5fxh5zgP2x1Q5A8WYAN8RJRVZb3lAReQQonkT9LVPPQ-2BlYuo-2BBE6sV8Vx4Kpb1rSyQknLng7YNBrcJVhIecHiQwrNPGzBTbniJs3V-2B4ymwqrJ9EfWI1M1a9a9SoFhSa84TTlDxIPSYSXZh0X4JtDU" style="line-height:normal" target="_blank"><img class="CToWUd" src="https://lh4.ggpht.com/8Na5eB_nsLqTkiT8FXXGMKUA6I6YYENThCLc93ekBIiNicKBDI1e_kOrr3rfL03lx6q6sr5I9e8ExlHWNtxK=s0#w=22&amp;h=22" style="margin-right:6px"></a>&nbsp;<a alt="LinkedIn" href="http://mailer.udacity.com/wf/click?upn=gmJQQrAKvQFtEwVr-2BF-2BO3njuu4hnI7Xb1WTYuoNLgAk-2FfLi5RVbwXBHby1xeAr1v0cKrF6pWzwpB1HzTIPcP88A3EkR-2FhgaAz7J8-2B23BCPP0cWA4jXmmdU9tqW7o5-2F-2FSZM5OEtEdy0NzgVySYVaMUQ7uHxFvT8rLUVMbjj-2FoqSehY-2BUuSzMa4GfimW9A3PP-2FFk42jPKFxKsuoopZzyt8O4K-2BoU37xTP1D3U7CPbYuN-2FeK4dLz63UDsXfIiFPlp70kBy-2BtAPxdOITf2sl7deybFr0OuQ45LMAUJYjGiFN7Qs2QV-2BGI4g0tXWcv3L-2Fv0jC_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgWUnKzlu5FfOncng5ueE35VZIcByvwdB8R5jMPXHvT-2BWSWi6sW8yXJSjvq5ZJ7Tjiwt-2Bc4kz8G4CQLOxwK5PPhoYU4IdiX-2B-2Bgqil-2BW3fXNkGjCPkdwmpfK-2F2WsNtMTmlU2f-2BSP7uWr-2F8tJOaWnJpCDqbyzs5jmpCI-2BcdjvBTrDVy" style="line-height:normal" target="_blank"><img class="CToWUd" src="https://lh4.ggpht.com/_A1j34SAIfFiQsEQkxY1vapIHst-seqUtj9iWeYSygxnQoFfXXCRsoiHxrdixArbMuC4R3ZLzsL1iTZ02g=s0#w=23&amp;h=22" style="margin-right:6px"></a><a href="http://mailer.udacity.com/wf/click?upn=PSxRa1Io7ndZdJAAeuIwmDMvY0gOsJP7BJaYok5lCKHtfFspIhs7jSiXxYDA5TvF-2F7i81-2Bp7IxRsFJICXy79wBZAZa9P9-2BAcPZXitY0P4G9B3rxBSiHsJEB48LBa6Qhv4VmxthD6FjlIEnLlTFWudRUyxnAwz1VTufUWl6gEeY0y9ruhQBt6xTo0nfw9SAP8_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgTplzgl04cTToyLCR5ov5dZTsnpED5OP-2B2bt5C0YXVBsPNuV6Y2OaE99a-2FrcPaleGucWEBIZRxDAcW-2BvMMY1O40u7Ya-2F1w6S8pgxuq-2Fzq3Ss9bw5NYtAAAhAQM175T0lIDyHU6mYyLFe5hc39OsLupkTJoKQMbAwwThMZ0NFRcjW" target="_blank"><img class="CToWUd" src="https://lh3.ggpht.com/OahVVOnbnVsq8bz7q3HlrwwhC_Xk38qyzI1_pj9U20wLSjJqcofTLRApwjCzJH_MCo7_BqfmaSxAt9xVJww=s0#w=22&amp;h=22" style="margin-right:6px"></a>
+
+								</td>
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+				<tr>
+					<td style="padding:27px 30px 17px" bgcolor="#f5f5f5">
+
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tbody><tr>
+								<td valign="top" width="170">
+									<table width="170" cellpadding="0" cellspacing="0">
+										<tbody><tr>
+											<td style="font:11px/11px Arial,Helvetica,sans-serif;color:#4e4e4e;padding:0 0 10px">
+												Explore <a style="color:#1192bf;text-decoration:none" href="http://mailer.udacity.com/wf/click?upn=2RL1KjfO9zPBjFPLhnA72NHnaq5GfnRyVURD2ZvfzjiLaDGX0x4PEqZRXIf0pUH2dDxfr5aBfpyBrxnBR8l2i5nZZ-2BhNQTh1q40xIq0NqncuE7DkRQKYuyq9dQXpYbwZjO3snLxF2bJWBmA-2BLHzQMg-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgSNpFdSi5-2Fu-2FZ-2BpYL5Sm-2BzW1KWG0f0KtP7-2FLDV3ZOQnaZ1fGn1ZmTNjD-2Baqh1r5xvM1848dZbHoTSTQ-2F7UIr9LRwsSdI-2B1DAZ1TFd9o94PpJGTpP5jcXMZLONVCIXEaLUdEjJFbkk2Gyk50-2BUMhLAI4xi6Ekup6R7t4hK2MsnHpf" target="_blank">Our Courses</a>
+											</td>
+										</tr>
+										<tr>
+											<td style="font:11px/11px Arial,Helvetica,sans-serif;color:#4e4e4e">
+												Visit <a style="color:#1192bf;text-decoration:none" href="http://mailer.udacity.com/wf/click?upn=7K-2F61dGjasOuK5tx9xztIW9NhoK6Bf8h2kQ6Akm6ZMpajReRBM6ZElFnT8xtsPODRG-2BRh5lLfyCPCP5oMQ1M-2FFvgEuBqRXPRV3xNYpqYPcXRoCD5KS4XLuNRYNF5eo3r_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgWJMnp3i3DKIqaJHGb7bKIZQPQ65y-2BYpIGWjIE-2BOTt5fFoUZEN5bcpyVUtmhoaGBVHZJfke3Twz7Tjukz-2FGHNn5kIWHwpnjb-2BfMkwXsSuFdS8Z1bCqsuUzgISNZIeHQpbGhUNKOTJqskd3Csevp6ND-2BneCzFSf8q5G8L9CPWaEER" target="_blank">Our Blog</a>
+											</td>
+										</tr>
+									</tbody></table>
+								</td>
+								<td width="10"></td>
+								<td valign="top" width="170">
+									<table width="170" cellpadding="0" cellspacing="0">
+										<tbody><tr>
+											<td style="font:11px/11px Arial,Helvetica,sans-serif;color:#4e4e4e;padding:0 0 9px">
+												Join our community
+											</td>
+										</tr>
+										<tr>
+											<td>
+												<table cellpadding="0" cellspacing="0">
+													<tbody><tr>
+														<td width="31">
+															<a href="http://mailer.udacity.com/wf/click?upn=mAr1hv7u9aVgF6aPgOb7rSEhb1pJK0WADpyxBRYlZL-2Bu4g1nGsV0fMpiN4S-2BKwD0FIz0uH-2FrcrokmpuRciAdflnQPbFq4T0YdNjURmuhfOZVprJrVXLOHaGxHXNne3sbBB2BJRmo6CfnFpRHvYya4Q-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgc7WO5XSLRcbxUp0YjfQMs2YrdkOoJ5Sjj1VqaJRVczGPsSfW3cAJiEBF-2FX8KtF6T3xw72Sml2ZPdotbpci6v-2Bnv6mxkDlrEBwZDYBLkD2cH2cZXDYWBfwhnKBtSBzpoaBfkTLqN98raIKVRwG0yM-2BUydNtEDcsGh7lgE1JkIWso" target="_blank"><img class="CToWUd" src="https://lh3.ggpht.com/Z-eq7OCAY6sujHCOY9ukFXESe4fixkS_oRQqE1IKXI3AVDICvruum9-nh0j7eBfR4pbXRo5WMAQxXRpolC8=s0#w=31&amp;h=31" style="vertical-align:top" alt="Facebook" height="31" width="31" border="0"></a>
+														</td>
+														<td width="9"></td>
+														<td width="31">
+															<a href="http://mailer.udacity.com/wf/click?upn=x-2B9jJRmsAVKxg1eJUbHBeKlcrNSucDbE-2Bh4unN8qFbyzxryiQvBAcwNARwenG81ohxFn-2BT7JGBsAumzpTL52TCsJZbaH53GCr6jnTxkyEIEYxWphaIVyC1u56HWTEVd9NiNNGYcLgY9LV0eGY8ymwg-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgahh1veDzHA63-2FX85BkNt-2Bmj1DDMG7ibjJ5X-2FZYvLoBqKXK84MJpWAyCosTauOWXIbAO9V1jBKJZHsRIkfO-2BpTZJyU9DU7cwelWqe0il6d1p-2Bq7-2B-2BCgWj68f2Xvpsezulkxyqiqs4AlRziNNbKSAWocDDyr2Fefz4wpdy9JB1yRS" target="_blank"><img class="CToWUd" src="https://lh5.ggpht.com/X_G5aptJQa0DY0eb6EswNThtipz-Fam2yQnUXzI1o-40gAL6p94xpH9B71sYBQ8b8tJp-RQnCEKfSLTfIw=s0#w=31&amp;h=31" style="vertical-align:top" alt="Google+" height="31" width="31" border="0"></a>
+														</td>
+														<td width="9"></td>
+														<td width="31">
+															<a href="http://mailer.udacity.com/wf/click?upn=K2HQgxYaqA24UVXjbtxtuZtvlXexx4KRKfP-2FSny5X0asKeKlUYWXDDdtATtIjVwPJy5BOcQ6D3tTczeSoudyZCOEQugw1rw1zBP7OtKvCg7tYmW8cwtfyNalY45Ee5yr5JOSHdeKd4dXvESXLO1WNw-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgQ-2F0DL-2FCq5VyTgfzEtRmHDsOO3xKfbHzg4bDwz-2FmjxlPjwTEug14hQl-2FOtFPlL6Vm-2B1A2nuWt-2BR5ytBxbL6AHkv6U-2Bbnp3zYOETUm9hg3qOIuY-2FK8c5VPGZJ4y4fwbEOZYFxVFaBYfio3SQj0xbP1zyLzF5M8gx-2Fsp7r6e1OKUit" target="_blank"><img class="CToWUd" src="https://lh3.ggpht.com/7cqv_H1UzQq3_-zvNRrJp1zI8ziPmh9StxZhPXMNwEPyEbXhuefAdKPfsnZOhSri-LylbCKTVQpmjd8dySM=s0#w=31&amp;h=31" style="vertical-align:top" alt="Twitter" height="31" width="31" border="0"></a>
+														</td>
+														<td width="9"></td>
+														<td width="31">
+															<a href="http://mailer.udacity.com/wf/click?upn=NysBH7NWJA8wUr20GZCLJ8ZQlJsqe0hnD8wHPAdWVZjlL0ncb-2B-2FAyqJn0ZUckBL16m-2FJTLcrKjH-2BLduRLqKM5czUSfRVqQ1H2rGwVOKU0iMaPx8COXfCYnxqp2r8DPBg3VOvb7YTV7rrAsEeb8Ll8Q-3D-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgfMwtbc-2BsqJoHIEn-2Fw4H3foyiG8LUeG4hqXHF-2FGthtZ8iJ-2BaNk78v4hgOFuKLavTFig-2BophfWysBFvMRe2WiEzbfGjxXioPYZWOn2oZxgyB6D-2BLeYmLvEi4PAIC8b6ijxlMjGZlnCf9kBQnsEXcb-2BP37KioLyifsMSo8IrC8d55S" target="_blank"><img class="CToWUd" src="https://lh6.ggpht.com/A9mEOneEk0Hqx7xgyYoLbEyHM3o8tq092fSHx9xz1awx2HQf3Gd1m6jhsqvG55WCFNXdMd8JflbpsWi9hw=s0#w=31&amp;h=31" style="vertical-align:top" alt="LinkedIn" height="31" width="31" border="0"></a>
+														</td>
+													</tr>
+												</tbody></table>
+											</td>
+										</tr>
+									</tbody></table>
+								</td>
+								<td width="40"></td>
+								<td valign="top" width="140">
+									<table width="140" cellpadding="0" cellspacing="0">
+										<tbody><tr>
+											<td style="font:11px/11px Arial,Helvetica,sans-serif;color:#4e4e4e;padding:0 0 10px">
+												Learn on mobile
+											</td>
+										</tr>
+										<tr>
+											<td style="padding:0 0 10px">
+												<a href="http://mailer.udacity.com/wf/click?upn=HnkdQnYtL5GcRgXVpV7XAI3vI9GU0bSeJjQHD-2F2BJVw4zZD0H7ToEDrsSPT3TEO-2F2RilfuP4OtxuWbS1D0d9ZAMvpDIkIYLYlqiu-2FZlkXEZHD9pYBB3Arxb7Xxpwps-2BDkTp5tJ5EMJzJ-2BFriT5tlXVxJFm069wLMUTKPlVgbBtw-3D_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgV33U8DMQLZW9z-2FyZaeJBvSpIpWN4WBRh33ZwmkT6dlwhuAkbiFKLLdku4dpZ-2Fcflh6SSmNYxNI6WBWtzZ1q9Imao6gMzf-2BHdmAEAz6Pyu5GHSKob0P59mgh-2Bineh2L9Su5Px4RRJvQESI3BUXXMieXhs-2BLNDUvmX8pTyg1SWVrt" target="_blank"><img class="CToWUd" src="https://lh6.ggpht.com/5mY-J8xINNmZ4x_6mbNYmhDWFT_qzG4q4QA2jPzRfiqRjUticf5u0eEEATtX04gbr_KrDSx8SDDBpGRHAWU=s0#w=300&amp;h=88" height="44" width="150" border="0"></a>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding:0 0 10px">
+												<a href="http://mailer.udacity.com/wf/click?upn=PSxRa1Io7ndZdJAAeuIwmCMrxq1iTOH2AlN4K6jlzfcW9rtgE-2BZbx2D9rCLOY6fouTDNgB03KA-2BBRzP8v-2B5tIZjDKkPOSDRg42BFdhcYpxfa50E9J-2FL3K4rnjO2HZcfVud8fRKdWlcls09VSu4YELUeHQ6knwFdzJpMmpigHt8SKglUEFe2sIw4rB0avAF4N_8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgawZJWqeM0lKm-2F6ti6Hj5ftHI2KHwfEb15LJIpU6E9aCKK4JJlYthMr9ythMA3XS-2BACleuPHTqQRjB3LNhCsmpXVFLOLlUwoKoHz4d4s9WTS7NAhOMDlok1jUsySR7-2FpWkY4Zho7KHlntehkszfAAFY8U8I0ExYfYXo8usFE94Ni" target="_blank"><img class="CToWUd" src="https://lh5.ggpht.com/Zb2s5Ho_mzDPnb9dBDIqQPpwFrxz1e902ksTnir8Cs_KdrYLvN_TLVK1pmKGbMPgolJ2wIlY23yI9OODDss=s0#w=300&amp;h=88" height="44" width="150" border="0"></a>
+											</td>
+										</tr>
+									</tbody></table>
+								</td>
+							</tr>
+						</tbody></table>
+
+					</td>
+				</tr>
+				<tr>
+					<td style="padding:21px 30px 30px">
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tbody><tr>
+								<td style="font:11px/13px Arial,Helvetica,sans-serif;color:#4e4e4e">
+									<br>
+									<span style="font-weight:bold">Udacity</span> 2465 Latham Street, Mountain View, CA 94040
+								</td>
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+			</tbody></table>
+		</td>
+	</tr>
+</tbody></table>
+
+<img class="CToWUd" src="https://ci6.googleusercontent.com/proxy/lJtdXMbJx1faUSN7W7-nBj3cyW4cpztEVKYVTaUAiQSk740Af-KJtaVJ8vTg1iRviHvv0luX15-2eKA_Y3hfwGGg003NH3ixd827nezVEV-nfVprEpRt_zr3GbaxQI8lv1xxVYhxQJC7-J3wlnI-aSSQsqU9Yv-8NAwl0s_MpM9sjCl4QsEaC3o8HosIRwffz5iLTwJ_IKhoIMuVFeWhZDkIXYmpMMI3eoAXBqbq1qq410JkeiAA9n0atNppiCgTzfZIVAAaGOVA011sdnW2AeV_iwjrI11H9Mc2fq-hjXg1BFhxZ5xz-asIoGodgWapddeEVT69BRGXBtQk6Yad5jaMbJr9dUK8BvhlsbJP28MDhEJpYc_-rnTpVt62WmfpUxs5K02p0uB1-YIdNnAoXl2MMG57wBP2eWVhYLIg5Nkfl3-OGUHh9xu0VR6D25jZ7rVTsuSXNtBMlDhZf3iyVBfrZ6jJ7pbTnbC_fowKEe81RCW-BYnDUdNroJYe0SQZsUZJnwbojMStMNd7pMF0OGRNU6A3pzB07SQ693E5u1vgswogDA=s0-d-e1-ft#http://mailer.udacity.com/wf/open?upn=8-2BDALT1gS1VbZ50yaO35K80bNKOdIwPn4X0e3ixumN0yy6DIf3N82o-2BRDelaVx51wqTeS1gIa7NZ5N3IJVaTOGqgU3H0S5A6x26Fp60-2BPWv1bIVyWTtjkZxpur9iNoTNpRBuxfyJDo57ptNaZ1kYgQxL4PFL-2FyxtAtRGjdlFr5XhOvsXz7yLlXDr0IrCCQfaoWNzfQgT7c7ZwptDfXki0fbYwSKWlWTWeTspS5nctQS3raYSpAraPVDqMX4luypqylw0g-2FR-2FIYpiLwDEzVneNcR1dH2U7QAgcVsLG0Z9h8BMQnhKGOTIaPtrlnVdX8OPGUOj7Y9BcZOaR2HUAtNkkQ-3D-3D" alt="" style="min-height:1px!important;width:1px!important;border-width:0!important;margin-top:0!important;margin-bottom:0!important;margin-right:0!important;margin-left:0!important;padding-top:0!important;padding-bottom:0!important;padding-right:0!important;padding-left:0!important" height="1" width="1" border="0"><div class="yj6qo"></div><div class="adL">
+</div>
