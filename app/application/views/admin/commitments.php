c969b62f48104c2f2c3bcad130523ed5fdd33821
@ProfNandaa <profnandaa@gmail.com>
2015-02-06 16:20:32 +0300

    Added way to view emails from admin side

--- app/application/views/admin/commitments.php
+++ app/application/views/admin/commitments.php
@@ -10,6 +10,12 @@
 </div>
 
 <div class="col-md-10">
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  View Emails
+</button>
+
+
 <table class="table">
 <tr>
 	<th width="2%">#</th>
@@ -39,5 +45,37 @@ foreach($cm->result() as $row){
 </table>
 
 </div>
+
+<!-- for manual emailing -->
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Emails</h4>
+        <p>For manual emailing, copy-paste and please use BCC for emailing</p>
+      </div>
+      <div class="modal-body form">
+        <textarea style="width:100%;">
+			<?php
+			$count = 0;
+			foreach($cm->result() as $row){
+				$count++;
+				if($count != 1) echo ",";
+				echo "$row->email";
+			}
+
+			?>
+			</textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 
 </div>
