8f9f615461536b553befe14f71c8a958788d8739
@ProfNandaa <profnandaa@gmail.com>
2015-04-10 10:56:11 +0300

    Fixed invite-someone link/btn, close #94

--- app/application/views/champion/reg_complete.php
+++ app/application/views/champion/reg_complete.php
@@ -7,11 +7,11 @@ Registration Completed
 		<?php $this->load->view("inc/progress_reg"); ?>
 
 
-		<div>
+	<div class="form">
 		<i class="fa fa-check-circle complete"></i> Thank you <strong><?php echo $this->session->userdata("first_name") ?></strong> for
 		registering as a champion. Please invite someone else to the platform.<br/><br/>
 		<?php echo anchor("champion/invite","<i class='fa fa-thumbs-o-up'></i> Invite Someone",
-			"class='btn btn-warning'"); ?>
+			"class='btn btn-lg btn-success'"); ?>
 
 			<?php echo " &nbsp; ".anchor("champion/profile","Skip <i class='fa fa-angle-double-right'></i> "); ?>
 	</div>
