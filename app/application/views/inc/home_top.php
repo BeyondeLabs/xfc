846fffc0fb11b8ed413fa0a3dbc4fc1fd5afb23f
@ProfNandaa <profnandaa@gmail.com>
2015-12-05 13:46:59 +0300

    Moved

--- app/application/views/inc/home_top.php
+++
@@ -1,7 +0,0 @@
-<div class="landing top">
-    <div class="container">
-        <h1>Partner with FOCUS today<br/>
-            <sub>and reach students!</sub>
-        </h1>
-    </div>
-</div>
--- app/application/views/inc/temp.php
+++
@@ -1,5 +0,0 @@
-<?php 
-#the template for loading the views
-$this->load->view("inc/header");
-$this->load->view($main);
-$this->load->view("inc/footer");
